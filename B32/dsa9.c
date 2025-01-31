da699e1e2d519f47146df85130fba254794c4277
Vanshika Rawat <one.shika16@gmail.com>
2025-01-31 20:55:53 +0530

    Add files via upload

---
+++ B32/dsa9.c
@@ -0,0 +1,28 @@
+//recurssive and iterative fnc diffference  create both 
+// 5 prac, 3 viva, 2 lab file 
+#include <stdio.h> 
+int search(int arr[], int n , int key){
+    for ( int i=0; i<n; i++){
+        if(arr[i] == key){
+            return i;
+        }
+    }
+    return -1;
+
+int main() {
+
+    int arr[] = {45, 32, 12, 67, 89};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int SearchKey = 12;
+    int index = search(arr, n, SearchKey);
+    if(index n-1) {
+        ("Element found at index ")
+    }
+}
+
+
+
+
+
+
+}
